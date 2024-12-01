f92c1fef2c1c149d4943aa1a2a325d1bbd07c67f
yayinsilalahi <yayinauliasilalahi@gmail.com>
2024-12-01 20:26:10 +0700

    Menerapkan Row dan Col Kedalam View Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,6 +39,23 @@
 
       <div class="container" style="margin-left:30px">
         <h1>Ini Halaman Home</h1>
+
+        <hr>
+        
+        <div class="row">
+            <div class="col-sm-6">
+                <h4>What is Lorem Ipsum?</h4>
+                <p style="text-align:justify">
+                    <b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+            <div class="col-sm-6">
+                <h4>Why do we use it?</h4>
+                <p style="text-align: justify">
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </p>
+            </div>
+        </div>
       </div>
 
 
