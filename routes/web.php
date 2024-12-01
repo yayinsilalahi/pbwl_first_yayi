06d27e92a263412ef9fc4e10b7dc05723950c8dd
yayinsilalahi <yayinauliasilalahi@gmail.com>
2024-12-01 19:45:49 +0700

    Installasi, pengenalan root, pengenalan view, bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    return view('index');
+});
+
+Route::get('about', function() {
+    return view('about');
+});
+
+Route::get('mahasiswa', function() {
+    return view('mahasiswa');
+});
+
+Route::get('profile', function() {
+    $nama = 'Yayi';
+    return view('profile', compact('nama'));
 });
