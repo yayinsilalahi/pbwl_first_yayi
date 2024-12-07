0f27f24c3b080a27a6b67e3d1c0f45525edf9d03
yayinsilalahi <yayinauliasilalahi@gmail.com>
2024-12-07 11:36:48 +0700

    Membuat model dan migration tabel mahasiswa ke dalam database

---
+++ database/migrations/2024_12_07_042705_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->integer('npm')->unique();
+            $table->string('nama_mahasiswa', 35)->nullable();
+            $table->enum('jk', ['L', 'P']);
+            $table->date('tgl_lahir')->nullable();
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
